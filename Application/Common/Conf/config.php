572627a7995da2c9b22c487820dca8718e62e66d
npt123 <1678730198@qq.com>
2018-08-28 10:40:21 +0800

    重新提交

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,7 +2,7 @@
 return array(
 	//'配置项'=>'配置值'
   'DB_TYPE'=>'mysql',
-  'DB_HOST'=>'127.0.0.1',
+  'DB_HOST'=>'101.132.97.42',
   'DB_NAME'=>'homestay',
   'DB_USER'=>'root',
   'DB_PWD'=>'123456',
