e1c1d7db9b6fbebd99df019a64bf08c515df05ee
Boziz <42742352+Boziz@users.noreply.github.com>
2018-08-31 17:23:06 +0800

    House

--- Application/House/Controller/HouseController.class.php
+++ Application/House/Controller/HouseController.class.php
@@ -323,6 +323,97 @@ class HouseController extends Controller
     );
     exit(json_encode($arr,JSON_UNESCAPED_UNICODE));
   }
+  public function AdmWatchHouse()
+  {
+    header('Access-Control-Allow-Origin:http://localhost:8081');
+    $ctime = date("Y-m-d H:i",time());
+    // 查看房源
+    // 封装返回的数据格式，注意以这个格式为统一格式，若有返回的数据全部封装在detail中
+    $find_order = M("house") -> select();
+    $ordernum = count($find_order);
+    for($i=0,$j=0;$i<$ordernum;$i++)
+    {
+      $order[$j][$i-6*$j] = $find_order[$i];
+      if(count($order[$j])>5)
+      {
+        $j++;
+      }
+    }
+    $arr = array(
+      "status" => 0,
+      "message" => "房源信息",
+      "timestamp" => $ctime,
+      "detail" => array($order)
+    );
+    exit(json_encode($arr,JSON_UNESCAPED_UNICODE));
+
+  }
+  public function AdmDateHouse()
+  {
+    header('Access-Control-Allow-Origin:http://localhost:8081');
+    $ctime = date("Y-m-d H:i",time());
+    // $ctime = "2018-09-26";
+
+    // 查看房态
+    // 封装返回的数据格式，注意以这个格式为统一格式，若有返回的数据全部封装在detail中
+    $House = M("house_order") -> where("FromDate >= '$ctime'") -> order('HouseId') -> select();
+    $j = 0;
+    $k = 0;
+    $i = 0;
+    $HouseDate[$j]["HouseId"] = $House[$i]["houseid"];
+    for($i=0;$i<count($House);$i++)
+    {
+
+      if($HouseDate[$j]["HouseId"] != $House[$i]["houseid"])
+      {
+        $k = 0;
+        /*
+        if($j == 0)
+        {
+          $HouseDate[$j]["HouseId"] = $House[$i]["houseid"];
+          $HouseDate[$j]["FromDate"][$k] = $House[$i]["fromdate"];
+          $HouseDate[$j]["ToDate"][$k] = $House[$i]["todate"];
+          $k++;
+        }
+        */
+
+        $j++;
+        $HouseDate[$j]["HouseId"] = $House[$i]["houseid"];
+        $HouseDate[$j]["FromDate"][$k] = $House[$i]["fromdate"];
+        $HouseDate[$j]["ToDate"][$k] = $House[$i]["todate"];
+        $k++;
+
+      }
+
+      else
+      {
+
+        $HouseDate[$j]["FromDate"][$k] = $House[$i]["fromdate"];
+        $HouseDate[$j]["ToDate"][$k] = $House[$i]["todate"];
+        $k++;
+      }
+    }
+
+
+
+    /*
+      $FromDate = M("house_order") -> where("FromDate >= '$ctime'") -> field("FromDate") ->select();
+      $ToDate = M("house_order") -> where("ToDate > '$ctime'") -> field("ToDate") ->select();
+      $HouseDate[0]["HouseId"] = $House[0]["houseid"];
+      $HouseDate[0]["FromDate"][0] = $House[0]["fromdate"];
+      $HouseDate[0]["ToDate"] = $House[0]["todate"];
+
+    */
+
+
+    $arr = array(
+      "status" => 0,
+      "message" => "房态信息",
+      "timestamp" => $ctime,
+      "detail" => array($HouseDate)
+    );
+    exit(json_encode($arr,JSON_UNESCAPED_UNICODE));
+  }
 }
 
 ?>
